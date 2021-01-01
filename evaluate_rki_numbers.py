cbf65b1e0c3a0477965e627854fdab76e57d79c0
Udo Hain <udo.hain@bluesilisk.de>
2021-01-01 12:57:53 +0100

    switched to csv file from https://npgeo-corona-npgeo-de.hub.arcgis.com

--- evaluate_rki_numbers.py
+++ evaluate_rki_numbers.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 
+# Datenquellen
+# https://npgeo-corona-npgeo-de.hub.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0/data?page=10
+# https://www.arcgis.com/home/item.html?id=f10774f1c63e40168479a1feb6c7ca74
+
 # Legende außerhalb: siehe https://pythonspot.com/matplotlib-legend/
 
 import argparse
-import pandas as pd
+import csv
 import numpy as np
 import matplotlib.pyplot as plt
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
+from collections import defaultdict
+import datetime
 
 parser = argparse.ArgumentParser(description='Analyze tasks in time sheet.')
 parser.add_argument("-i", "--inputfile", required=True, type=str, help='name of time sheet file')
@@ -15,47 +21,46 @@ parser.add_argument("-i", "--inputfile", required=True, type=str, help='name of 
 args = vars(parser.parse_args())
 reportfile = args["inputfile"]
 
-rki_sheets = pd.read_excel(reportfile, engine="openpyxl", sheet_name='BL_7-Tage-Fallzahlen', header=None)
-contents = rki_sheets.to_dict(orient='record')
 i = 0
 tage = []
+#countries = defaultdict(lambda: "")
 countries = {}
 nof_rows = 0
-for row in contents:
-    i += 1
-    if i == 1:
-        continue
-    if i == 2:
-        print(row[0])
-        continue
-    if i == 3:
-        nof_rows = len(row)
-        for j in range(1, nof_rows):
-            tage.append(row[j])
-        # print(tage)
-        continue
-    country = row[0]
-    fallzahlen = []
-    nof_rows = len(row)
-    last_value = 0
-    for j in range(1, nof_rows):
-        if type(row[j]) != pd._libs.tslibs.nattype.NaTType:
-            last_value = last_value + row[j]/7
-            fallzahlen.append(last_value)
-    countries[country] = fallzahlen
-    # print(countries[country])
-
+with open(reportfile, newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        i += 1
+        if i == 1:
+            continue
+        country = row[2]
+        cnt_infect = int(row[6])
+        cnt_death = int(row[7])
+        cnt_date = datetime.datetime.strptime(row[8], '%Y/%m/%d %H:%M:%S')
+        # print(type(row[8]), type(cnt_date))
+        # print(country, cnt_infect, cnt_death, cnt_date)
+        if country not in countries:
+            countries[country] = {}
+        if not cnt_date in countries[country]:
+            countries[country][cnt_date] = cnt_infect
+        else:
+            countries[country][cnt_date] += cnt_infect
 
 for country in countries:
-    if country == 'Gesamt':
-        continue
     nof_rows = len(countries[country])
-    days_to_plot = tage[0:nof_rows]
-    #print(nof_rows, len(countries[country]))
+    # print(country, len(countries[country]), countries[country])
+    tage = []
+    infects = []
+    last_sum = 0
+    for key in sorted(countries[country]):
+        # print(key, countries[country][key])
+        tage.append(key)
+        last_sum += countries[country][key]
+        infects.append(last_sum)
     #print(countries[country])
     # x = np.linspace(1, nof_rows, nof_rows)
-    y = np.array(countries[country])
-    plt.plot(days_to_plot, y, label=country)
+    y = np.array(infects)
+    #plt.scatter(tage, y, label=country)
+    plt.plot(tage, y, label=country)
 
 plt.legend()
 plt.show()
